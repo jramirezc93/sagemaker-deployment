3831190e8cf63ca2a2e19062d048a74106c85394
jramirezc93 <jorgerc93@msn.com>
2020-08-22 17:59:33 +0200

    feat: Adding project preprocessing and model

--- Project/serve/predict.py
+++ Project/serve/predict.py
@@ -69,15 +69,13 @@ def predict_fn(input_data, model):
     #       You should produce two variables:
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
-
-    data_X = None
-    data_len = None
+    data_X, data_len = convert_and_pad(word_dict = model.word_dict, sentence = review_to_words(input_data), pad= 500)
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
     data_pack = np.hstack((data_len, data_X))
     data_pack = data_pack.reshape(1, -1)
-    
+
     data = torch.from_numpy(data_pack)
     data = data.to(device)
 
@@ -86,7 +84,7 @@ def predict_fn(input_data, model):
 
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
-
-    result = None
+    with torch.no_grad():
+        result = int(np.round(model.forward(data).numpy()))
 
     return result
